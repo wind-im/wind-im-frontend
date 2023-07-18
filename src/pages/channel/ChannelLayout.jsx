f18044e27460b517b840a6ec866f070dbf4d857e
sawyerrick <1103031078@qq.com>
2023-07-18 21:27:35 +0800

    feat: close channel menu when clicking elsewhere

--- src/pages/channel/ChannelLayout.jsx
+++ src/pages/channel/ChannelLayout.jsx
@@ -2,221 +2,236 @@ import { beOfflineInChannel, beOnlineInChannel, deleteChannel, getChannelInfo, g
 import Link from 'next/link'
 import { useMutation, useQuery } from 'react-query'
 import { AiOutlineNumber, AiOutlineMenu } from 'react-icons/ai'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Avatar from '@/components/Avatar'
 
-export default function ChannelLayout ({ children, channelId }) {
-  // handle being online or offline in a channel
-  useChannelOnlineStatus(channelId)
+export default function ChannelLayout({ children, channelId }) {
+    // handle being online or offline in a channel
+    useChannelOnlineStatus(channelId)
 
-  return (
-    <div className='h-full w-full bg-[#25272a] flex'>
-      <ChannelSidebar channelId={channelId} />
-      {children}
-      <ChannelRightSidebar channelId={channelId} />
-    </div>
-  )
+    return (
+        <div className='h-full w-full bg-[#25272a] flex'>
+            <ChannelSidebar channelId={channelId} />
+            {children}
+            <ChannelRightSidebar channelId={channelId} />
+        </div>
+    )
 }
 
-function useChannelOnlineStatus (channelId) {
-  useEffect(() => {
-    beOnlineInChannel(channelId)
-    async function notifyOffline () {
-      console.log('Being offline in channel:' + channelId)
-      beOfflineInChannel(channelId)
-    }
-    function onBeforeUnload (event) {
-      notifyOffline()
-    }
-    addEventListener('beforeunload', onBeforeUnload)
-    return () => {
-      notifyOffline()
-      removeEventListener('beforeunload', onBeforeUnload)
-    }
-  }, [channelId])
+function useChannelOnlineStatus(channelId) {
+    useEffect(() => {
+        beOnlineInChannel(channelId)
+        async function notifyOffline() {
+            console.log('Being offline in channel:' + channelId)
+            beOfflineInChannel(channelId)
+        }
+        function onBeforeUnload(event) {
+            notifyOffline()
+        }
+        addEventListener('beforeunload', onBeforeUnload)
+        return () => {
+            notifyOffline()
+            removeEventListener('beforeunload', onBeforeUnload)
+        }
+    }, [channelId])
 }
 
-function ChannelSidebar ({ channelId }) {
-  const deleteChannelMut = useMutation('deleteChannel', deleteChannel)
-  const leaveChannelMut = useMutation('leaveChannel', leaveChannel)
+function ChannelSidebar({ channelId }) {
+    const deleteChannelMut = useMutation('deleteChannel', deleteChannel)
+    const leaveChannelMut = useMutation('leaveChannel', leaveChannel)
 
-  function onClickDeleteChannel () {
-    if (window.confirm('Do you really want to delete this channel?')) {
-      deleteChannelMut.mutate({ channelId })
+    function onClickDeleteChannel() {
+        if (window.confirm('Do you really want to delete this channel?')) {
+            deleteChannelMut.mutate({ channelId })
+        }
     }
-  }
 
-  function onClickLeaveChannel () {
-    if (window.confirm('Do you really want to leave this channel?')) {
-      leaveChannelMut.mutate({ channelId })
+    function onClickLeaveChannel() {
+        if (window.confirm('Do you really want to leave this channel?')) {
+            leaveChannelMut.mutate({ channelId })
+        }
     }
-  }
 
-  return (
-    <>
-      <div className="shrink-0 flex flex-col h-full w-64 border-r-[1px] border-solid border-r-[#323437] overflow-y-hidden">
-        <ChannelInfoPanel channelId={channelId}/>
-        <TextPannel channelId={channelId}/>
-        <VoicePannel channelId={channelId}/>
-        {/* <div className='mt-auto flex flex-col space-y-3'>
+    return (
+        <>
+            <div className="shrink-0 flex flex-col h-full w-64 border-r-[1px] border-solid border-r-[#323437] overflow-y-hidden">
+                <ChannelInfoPanel channelId={channelId} />
+                <TextPannel channelId={channelId} />
+                <VoicePannel channelId={channelId} />
+                {/* <div className='mt-auto flex flex-col space-y-3'>
           <button className='bg-gray-400 text-white rounded-md' onClick={onClickLeaveChannel}>Leave Channel</button>
           <button className='bg-red-700 text-white rounded-md' onClick={onClickDeleteChannel}>Delete Channel</button>
         </div> */}
-      </div>
-    </>
-  )
-}
-
-function ChannelRightSidebar ({ channelId }) {
-  const { data, error, isLoading } = useQuery(['getChannelMembers', channelId], () => getChannelUserInfo(channelId))
-
-  return (
-    <div className='ml-auto overflow-hidden w-[300px] border-l-[1px] border-solid border-l-[#323437] p-3 text-white'>
-      <div>
-        <div>ONLINE - {data ? data.data?.onlineUserCnt : 0}</div>
-        {data != null && data.data?.members?.filter(m => m.online).map((member) => {
-          return (
-            <div key={member.uid} className='flex my-1 items-center space-x-3 p-2 hover:bg-gray-600 rounded-lg'>
-              <Avatar username={member.userRel?.username} />
-              <p className='font-bold'>{member.userRel?.username}</p>
             </div>
-          )
-        })}
-      </div>
-      <div>
-        <div>OFFLINE - {data ? data.data?.offlineUserCnt : 0}</div>
-        {data != null && data.data?.members?.filter(m => !m.online).map((member) => {
-          return (
-            <div key={member.uid} className='flex my-1 items-center space-x-3 p-2 hover:bg-gray-600 rounded-lg'>
-              <Avatar username={member.userRel?.username} />
-              <p className='font-bold'>{member.userRel?.username}</p>
+        </>
+    )
+}
+
+function ChannelRightSidebar({ channelId }) {
+    const { data, error, isLoading } = useQuery(['getChannelMembers', channelId], () => getChannelUserInfo(channelId))
+
+    return (
+        <div className='ml-auto overflow-hidden w-[300px] border-l-[1px] border-solid border-l-[#323437] p-3 text-white'>
+            <div>
+                <div>ONLINE - {data ? data.data?.onlineUserCnt : 0}</div>
+                {data != null && data.data?.members?.filter(m => m.online).map((member) => {
+                    return (
+                        <div key={member.uid} className='flex my-1 items-center space-x-3 p-2 hover:bg-gray-600 rounded-lg'>
+                            <Avatar username={member.userRel?.username} />
+                            <p className='font-bold'>{member.userRel?.username}</p>
+                        </div>
+                    )
+                })}
             </div>
-          )
-        })}
-      </div>
-    </div>
-  )
-}
-
-function ChannelInfoPanel ({ channelId }) {
-  const { data, isLoading, error } = useQuery(['getChannelInfo', channelId], () => getChannelInfo(channelId))
-  const [manueActive, setManueActive] = useState(false)
-  function onMenuClick () {
-    console.log('menu click')
-    setManueActive(!manueActive)
-  }
-
-  return (
-    <div className='relative'>
-      <div className="p-4 text-[#e6eaf0] font-bold text-xl flex items-center border-b-[1px] border-[#323437]">
-        {isLoading && <p >Anonymous</p>}
-        {data && <p>{data.data?.name}</p>}
-        <button onClick={onMenuClick} className='ml-auto hover:cursor-pointer'><AiOutlineMenu size={20} /></button>
-      </div>
-      {manueActive && <ChannelMenu channelId={channelId} />}
-    </div>
-
-  )
-}
-
-function ChannelMenu ({ channelId }) {
-  function onClickInvitePeople () {
-    // open modal
-    const dialog = document.querySelector('dialog')
-    dialog.showModal() // Opens a modal
-    // close when clicking else where
-    dialog.addEventListener('click', e => {
-      const dialogDimensions = dialog.getBoundingClientRect()
-      if (
-        e.clientX < dialogDimensions.left ||
-        e.clientX > dialogDimensions.right ||
-        e.clientY < dialogDimensions.top ||
-        e.clientY > dialogDimensions.bottom
-      ) {
-        dialog.close()
-      }
-    })
-  }
-
-  return (
-    <div className='absolute flex w-full px-7'>
-      <div className='flex flex-col bg-[rgb(23,24,26)] rounded-md w-full text-gray-200 p-3'>
-        <span className='hover:cursor-pointer p-1 hover:bg-[#3b3c3f] rounded-lg' onClick={onClickInvitePeople}>Invite People</span>
-        <span className='hover:cursor-pointer p-1 hover:bg-[#3b3c3f] rounded-lg'>Channel Settings</span>
-      </div>
-      <dialog className='backdrop:bg-[rgba(0,0,0,0.5)] w-96 h-48 rounded-lg'>
-        <InviteDialogContent channelId={channelId}/>
-      </dialog>
-    </div>
-  )
-}
-
-function InviteDialogContent ({ channelId }) {
-  // request api for a invite url
-  const { data, isLoading, error } = useQuery(['getInviteUrl', channelId], () => getInviteUrl(channelId))
-  console.log(data?.data)
-
-  return (
-    <div className=''>
-      <span>Invite friends to this channel.</span>
-      {/* display invite url */}
-      <div className='flex items-center space-x-3'>
-        <div className='w-full p-1 rounded-md'> {data?.data?.inviteUrl}</div>
-        <button className='bg-gray-400 text-white rounded-md p-1'>Copy</button>
-      </div>
-    </div>
-  )
-}
-
-function TextPannel ({ channelId }) {
-  const { data, isLoading, error } = useQuery(['getRoomList', channelId], () => getRoomList(channelId))
-  return (
-    <div className='p-4 mt-3'>
-      <p className='text-[#e6eaf0] text-sm'>TEXT ROOMS</p>
-      <div className='ml-4 mt-1'>
-        {data?.data?.map((room) => {
-          return (
-            <TextPannelSelection key={room.id} name={room.name} to={`/channel/${channelId}/${room.id}`}/>
-          )
-        })}
-      </div>
-    </div>
-  )
-}
-
-function TextPannelSelection ({ icon, name, cnt, to }) {
-  return (
-    <>
-      <Link href={to}>
-        <div className='text-gray-400 p-1 rounded-md hover:bg-[#3b3c3f] hover:cursor-pointer'>
-          <div className='flex items-center'>
-            <AiOutlineNumber size={24}/>
-            <span>{name}</span>
-          </div>
+            <div>
+                <div>OFFLINE - {data ? data.data?.offlineUserCnt : 0}</div>
+                {data != null && data.data?.members?.filter(m => !m.online).map((member) => {
+                    return (
+                        <div key={member.uid} className='flex my-1 items-center space-x-3 p-2 hover:bg-gray-600 rounded-lg'>
+                            <Avatar username={member.userRel?.username} />
+                            <p className='font-bold'>{member.userRel?.username}</p>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
-      </Link>
-    </>
-  )
+    )
 }
 
-function VoicePannel () {
-  const { error, isLoading, data } = useQuery('getPrivateMsg', getPrivateMsg)
-  return (
-    <div className='p-4'>
-      <p className='text-[#e6eaf0] text-sm mt-2'>VOICE ROOMS</p>
-      <div className='ml-4 mt-1'>
-        {/* <VoiceSelection name="Default" to='/channel/voice'/> */}
-      </div>
-    </div>
-  )
+function ChannelInfoPanel({ channelId }) {
+    const { data, isLoading, error } = useQuery(['getChannelInfo', channelId], () => getChannelInfo(channelId))
+    const [menuActive, setMenuActive] = useState(false)
+    function onMenuClick() {
+        console.log('menu click')
+        setMenuActive(!menuActive)
+    }
+
+    const menuElement = useRef(null)
+    const closeOpenMenus = (e) => {
+        if (menuElement.current && menuActive && !menuElement.current.contains(e.target)) {
+            setMenuActive(false)
+        }
+    }
+
+    useEffect(() => {
+        const closeMenuEvent = document.addEventListener('mousedown', closeOpenMenus)
+        return () => {
+            document.removeEventListener('mousedown', closeMenuEvent)
+        }
+    }, [])
+
+    return (
+        <div className='relative'>
+            <div className="p-4 text-[#e6eaf0] font-bold text-xl flex items-center border-b-[1px] border-[#323437]">
+                {isLoading && <p >Anonymous</p>}
+                {data && <p>{data.data?.name}</p>}
+                <button onClick={onMenuClick} className='ml-auto hover:cursor-pointer'><AiOutlineMenu size={20} /></button>
+            </div>
+            {menuActive && <ChannelMenu menuElement={menuElement} channelId={channelId} />}
+        </div>
+
+    )
 }
 
-function VoiceSelection ({ icon, name, cnt, to }) {
-  return (
-    <>
-      <Link href={to}>
-        <div className='text-gray-400 p-1 rounded-md hover:bg-[#3b3c3f] hover:cursor-pointer'>{name}</div>
-      </Link>
-    </>
-  )
+function ChannelMenu({ channelId, menuElement }) {
+    function onClickInvitePeople() {
+        // open modal
+        const dialog = document.querySelector('dialog')
+        dialog.showModal() // Opens a modal
+        // close when clicking else where
+        dialog.addEventListener('click', e => {
+            const dialogDimensions = dialog.getBoundingClientRect()
+            if (
+                e.clientX < dialogDimensions.left ||
+                e.clientX > dialogDimensions.right ||
+                e.clientY < dialogDimensions.top ||
+                e.clientY > dialogDimensions.bottom
+            ) {
+                dialog.close()
+            }
+        })
+    }
+
+
+    return (
+        <div ref={menuElement} className='absolute flex w-full px-7'>
+            <div className='flex flex-col bg-[rgb(23,24,26)] rounded-md w-full text-gray-200 p-3'>
+                <span className='hover:cursor-pointer p-1 hover:bg-[#3b3c3f] rounded-lg font-bold' onClick={onClickInvitePeople}>Invite People</span>
+                <span className='hover:cursor-pointer p-1 hover:bg-[#3b3c3f] rounded-lg font-bold'>Channel Settings</span>
+            </div>
+            <dialog className='backdrop:bg-[rgba(0,0,0,0.5)] w-96 h-48 rounded-lg'>
+                <InviteDialogContent channelId={channelId} />
+            </dialog>
+        </div>
+    )
+}
+
+function InviteDialogContent({ channelId }) {
+    // request api for a invite url
+    const { data, isLoading, error } = useQuery(['getInviteUrl', channelId], () => getInviteUrl(channelId))
+    console.log(data?.data)
+
+    return (
+        <div className=''>
+            <span>Invite friends to this channel.</span>
+            {/* display invite url */}
+            <div className='flex items-center space-x-3'>
+                <div className='w-full p-1 rounded-md'> {data?.data?.inviteUrl}</div>
+                <button className='bg-gray-400 text-white rounded-md p-1'>Copy</button>
+            </div>
+        </div>
+    )
+}
+
+function TextPannel({ channelId }) {
+    const { data, isLoading, error } = useQuery(['getRoomList', channelId], () => getRoomList(channelId))
+    return (
+        <div className='p-4 mt-3'>
+            <p className='text-[#e6eaf0] text-sm'>TEXT ROOMS</p>
+            <div className='ml-4 mt-1'>
+                {data?.data?.map((room) => {
+                    return (
+                        <TextPannelSelection key={room.id} name={room.name} to={`/channel/${channelId}/${room.id}`} />
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+function TextPannelSelection({ icon, name, cnt, to }) {
+    return (
+        <>
+            <Link href={to}>
+                <div className='text-gray-400 p-1 rounded-md hover:bg-[#3b3c3f] hover:cursor-pointer'>
+                    <div className='flex items-center'>
+                        <AiOutlineNumber size={24} />
+                        <span>{name}</span>
+                    </div>
+                </div>
+            </Link>
+        </>
+    )
+}
+
+function VoicePannel() {
+    const { error, isLoading, data } = useQuery('getPrivateMsg', getPrivateMsg)
+    return (
+        <div className='p-4'>
+            <p className='text-[#e6eaf0] text-sm mt-2'>VOICE ROOMS</p>
+            <div className='ml-4 mt-1'>
+                {/* <VoiceSelection name="Default" to='/channel/voice'/> */}
+            </div>
+        </div>
+    )
+}
+
+function VoiceSelection({ icon, name, cnt, to }) {
+    return (
+        <>
+            <Link href={to}>
+                <div className='text-gray-400 p-1 rounded-md hover:bg-[#3b3c3f] hover:cursor-pointer'>{name}</div>
+            </Link>
+        </>
+    )
 }
--- src/pages/home/friend/AddFriendWindow.jsx
+++ src/pages/home/friend/AddFriendWindow.jsx
@@ -2,51 +2,52 @@ import axios from '@/utils/axiosUtils'
 import { useRef } from 'react'
 import { useMutation } from 'react-query'
 
-async function addFriend ({ usernameAndTag }) {
-  const result = await axios.post('/api/friendRequest', {
-    usernameAndTag,
-    opType: 0,
-    content: 'You have a new friend request.'
-  })
-  return result.data
+async function addFriend({ usernameAndTag }) {
+    const result = await axios.post('/api/friendRequest', {
+        usernameAndTag,
+        opType: 0,
+        content: 'You have a new friend request.'
+    })
+    return result.data
 }
 
 // todo 添加动效
-export default function AddFriendWindow ({ openAddFriendWindow, setOpenAddFriendWindow }) {
-  const bgElementId = 'addFriendWindow'
-  const cancelElementId = 'cancelButton'
-  const addFriendMutation = useMutation(addFriend)
-  const $usernameAndTag = useRef(null)
+export default function AddFriendWindow({ openAddFriendWindow, setOpenAddFriendWindow }) {
+    const bgElementId = 'addFriendWindow'
+    const cancelElementId = 'cancelButton'
+    const addFriendMutation = useMutation(addFriend)
+    const $usernameAndTag = useRef(null)
 
-  function onClickCloseWindow (e) {
-    if (e.target.id === bgElementId || e.target.id === cancelElementId) {
-      setOpenAddFriendWindow(false)
+    function onClickCloseWindow(e) {
+        if (e.target.id === bgElementId || e.target.id === cancelElementId) {
+            setOpenAddFriendWindow(false)
+        }
     }
-  }
 
-  function onClickSubmit () {
-    const usernameAndTag = $usernameAndTag.current.value
-    addFriendMutation.mutate({ usernameAndTag })
-    // setOpenAddFriendWindow(false)
-  }
+    function onClickSubmit() {
+        const usernameAndTag = $usernameAndTag.current.value
+        addFriendMutation.mutate({ usernameAndTag })
+        // setOpenAddFriendWindow(false)
+    }
 
-  if (!openAddFriendWindow) {
-    return <></>
-  }
+    if (!openAddFriendWindow) {
+        return <></>
+    }
 
-  return (
-    <div id={bgElementId} className='fixed z-20 flex items-center w-full h-full left-0 top-0 bg-[rgba(0,0,0,0.5)]' onClick={onClickCloseWindow}>
-      <div className="mx-auto flex flex-col w-[440px] h-[231px] bg-[#25262a] rounded-2xl p-5 ">
-        <h1 className="text-white text-xl font-bold">ADD FRIEND</h1>
-        <p className="text-[#717579] mt-2">You can add a friend with their tag.</p>
-        <input type="text" className="m-4 text-white rounded-xl p-2 border-solid border-2 border-[#323437] focus:border-[#6bc001] bg-transparent" placeholder="Enter a Username#Tag" ref={$usernameAndTag}></input>
-        {addFriendMutation.error && <div className='ml-4 text-red-600'>{addFriendMutation.error.response.data.message}</div>}
-        {addFriendMutation.data && <div className='ml-4 text-white'>{JSON.stringify(addFriendMutation.data)}</div>}
-        <div className='flex ml-auto mt-auto gap-3 text-white'>
-          <button id='cancelButton' className=''>Cancel</button>
-          <button className='rounded-md bg-[#6bc001] p-1' onClick={onClickSubmit}>Send Friend Request</button>
+    return (
+        <div id={bgElementId} className='fixed z-20 flex items-center w-full h-full left-0 top-0 bg-[rgba(0,0,0,0.5)]'
+            onClick={onClickCloseWindow}>
+            <div className="mx-auto flex flex-col w-[440px] h-[231px] bg-[#25262a] rounded-2xl p-5 ">
+                <h1 className="text-white text-xl font-bold">ADD FRIEND</h1>
+                <p className="text-[#717579] mt-2">You can add a friend with their tag.</p>
+                <input type="text" className="m-4 text-white rounded-xl p-2 border-solid border-2 border-[#323437] focus:border-[#6bc001] bg-transparent" placeholder="Enter a Username#Tag" ref={$usernameAndTag}></input>
+                {addFriendMutation.error && <div className='ml-4 text-red-600'>{addFriendMutation.error.response.data.message}</div>}
+                {addFriendMutation.data && <div className='ml-4 text-white'>{JSON.stringify(addFriendMutation.data)}</div>}
+                <div className='flex ml-auto mt-auto gap-3 text-white'>
+                    <button id='cancelButton' className=''>Cancel</button>
+                    <button className='rounded-md bg-[#6bc001] p-1' onClick={onClickSubmit}>Send Friend Request</button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
